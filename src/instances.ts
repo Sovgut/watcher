35181714b2d815650fdcefbb8f6110a484901d1c
Sergey Sovgut <sergey.sovgut@outlook.com>
2024-01-14 16:10:52 +0200

    Update package version and modify REQUEST_FUNCTION

--- src/instances.ts
+++ src/instances.ts
@@ -1,11 +1,14 @@
+import axios from "axios";
 import {Database} from "source:database";
 import {Observer} from "source:observer";
 import {Pipeline} from "source:pipeline";
 import {Scheduler} from "source:scheduler";
-import {requestWorker} from "source:workers";
 
 export const scheduler = new Scheduler();
 export const observer = new Observer();
 export const pipeline = new Pipeline();
 export const database = new Database();
-export let REQUEST_FUNCTION: (url: string) => Promise<string | undefined> = requestWorker.request;
+export let REQUEST_FUNCTION: (url: string) => Promise<string | undefined> = async function (url: string) {
+	const response = await axios.get(url, {responseType: "text"});
+	return response.data;
+};
--- src/workers/index.ts
+++
@@ -1,7 +0,0 @@
-import {wrap} from "comlink";
-
-import type {RequestWorker} from "source:workers/request.worker";
-// @ts-ignore
-import RequestInstanceWorker from "source:workers/request.worker?worker&inline";
-
-export const requestWorker = wrap(new RequestInstanceWorker()) as RequestWorker;
